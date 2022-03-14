7f1ce3cf04ecd21188425852754400ab237cacf3
Alberto Reyes <agrn96p@gmail.com>
2022-03-13 23:32:21 -0600

    3er Commit Fase 2
    Creacion de Lista Doblemente Enlazada

---
+++ EDD_PROYECTO_FASE2_201612174/src/edd_proyecto_fase2_201612174/Lista_DE.java
@@ -0,0 +1,43 @@
+package edd_proyecto_fase2_201612174;
+
+/**
+ * @author 201612174 --Alberto Gabriel Reyes Ning
+ */
+public class Lista_DE {
+    Node inicio, fin;
+    
+    public Lista_DE(){
+        this.inicio = null;
+        this.fin = null;
+    }
+    public void add() {
+        Node newNode = new Node();
+        if (inicio == null) { //Agrega al inicio
+            inicio = newNode;
+        } else if (inicio.next == inicio) { //Agregar despues del inicio
+            inicio.next = newNode;
+            newNode.prev = inicio;
+        } else { //Agregar al ultimo del circulo
+            Node current = inicio;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+            newNode.prev = current;
+        }
+    }
+
+    public void display() {
+        Node current = inicio;
+        if (inicio == null) {
+            System.out.println("Lista esta vacio");
+        } else {
+            System.out.println("Nodos son ");
+            do {
+                System.out.println("Placeholder");
+                current = current.next;
+            } while (current != inicio);
+            System.out.println();
+        }
+    }
+}
